1ce42ff722608f9ebc9f450fdffe94a33be818c0
Michal Stanke <michal@stanke.cz>
2018-03-18 10:53:44 +0100

    Swallow exit code 1 from hg push

--- push-central-to-github.sh
+++ push-central-to-github.sh
@@ -6,5 +6,6 @@ cd ./github_l10n-mozilla-central-cs
 hg pull -u central
 hg update
 hg bookmark -f default
-hg push github
+hg push github || push_exit_code=$?
 cd -
+(($push_exit_code==0 || $push_exit_code==1)) || exit $push_exit_code
